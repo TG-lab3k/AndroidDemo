ad67f09cf3b95aadac2e0fa0a4aae2992d028a3a
tonyley <tonyley@gmail.com>
2013-10-11 17:28:05 +0800

    init and add custom view demo

---
+++ src/com/example/demo/ui/custom/Custom1View.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.example.demo.ui.custom;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author tony.ley
+ *
+ */
+public class Custom1View extends View {
+
+	public Custom1View(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+}
---
+++ src/com/example/demo/ui/custom/CustomFragment.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.example.demo.ui.custom;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * @author tony.ley
+ *
+ */
+public class CustomFragment extends Fragment {
+
+}
